1d127454c8130992bc02e8a8e656507236755091
Kevin Vargas <vargaskevv@gmail.com>
2020-06-17 06:54:04 -0400

    fixed config file in main.go and dockerfile

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/vkevv/back-rectifier/pkg/api"
 	"github.com/vkevv/back-rectifier/pkg/config"
 )
 
 func main() {
-	conf, err := config.GetConfig("./cmd/api/config.yaml")
+	projectDir, err := os.Getwd()
+	checkError(err)
+	conf, err := config.GetConfig(projectDir + "/cmd/api/config.yaml")
 	checkError(err)
 	checkError(api.StartAPI(conf))
 }
